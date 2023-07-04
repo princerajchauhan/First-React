17fd2b25158969b346d35324ccdfc8dffda9f922
Prince <princeraj.chauhan@outlook.com>
2023-07-04 22:23:23 +0530

    Initial React completer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types'
 export default function Navbar(props) {
     return (
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-            <a className="navbar-brand" href="/">{props.title}</a>
+            <a className="navbar-brand" href="#">{props.title}</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -15,7 +15,7 @@ export default function Navbar(props) {
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         {/* <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link> */}
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
                     {/* <li className="nav-item">
                         <Link className="nav-link" to="/about">{props.aboutText}</Link>
